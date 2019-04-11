3457dfb7d4d4508557e5e3c81a76dca4943732c9
Gabriel Chelariu <gabrielchelariu@outlook.com>
2019-04-11 18:29:53 +0300

    Update (Nu am reusit ex4)

--- Modul4/src/main/java/TEME/temaStack/StringStack.java
+++ Modul4/src/main/java/TEME/temaStack/StringStack.java
@@ -2,17 +2,17 @@ package TEME.temaStack;
 
 public class StringStack {
     private int MAX_SIZE;
-    private String[] stack ;
+    private String[] stack;
     private int top;
 
-    public StringStack(int arraySize){
+    public StringStack(int arraySize) {
         this.MAX_SIZE = arraySize;
         stack = new String[MAX_SIZE];
         top = -1;
     }
 
-    void push(String val){
-        if (top == MAX_SIZE - 1){
+    void push(String val) {
+        if (top == MAX_SIZE - 1) {
             return;
         }
         top++;
@@ -28,17 +28,25 @@ public class StringStack {
         return val;
     }
 
-    void method(String myMethode){
+    void method(String myMethode) {
         String[] desparte = myMethode.split(" ");
-        for(String element : desparte ){
+        for (String element : desparte) {
             push(element);
         }
-        for(String element : desparte){
+        for (String element : desparte) {
             System.out.print(pop() + " ");
         }
     }
 
-//    void reverse(String){
-//
-//    }
+    void reverse(String string) {
+        int size = string.length();
+        String[] desparte = new String[size];
+        for (int i = 1; i < string.length(); i++) {
+            System.out.println(desparte[i]);
+        }
+        for (String element : desparte) {
+            push(element);
+            System.out.print(pop() + " ");
+        }
+    }
 }
--- Modul4/src/main/java/TEME/temaStack/stackGabriel.java
+++ Modul4/src/main/java/TEME/temaStack/stackGabriel.java
@@ -7,21 +7,24 @@ public class stackGabriel {
 //        iCanDoIt();
 //        ex2Impossible();
 //        ex3ReverseConsola();
+
+//        StringStack cuvant = new StringStack(1);
+//        cuvant.reverse("atiatab");
+//    }
+
+//    private static void ex3ReverseConsola() {
+//        StringStack myMethod = new StringStack(10);
+//        Scanner s = new Scanner(System.in);
+//        String myString = s.nextLine();
+//        myMethod.method(myString);
     }
 
-    private static void ex3ReverseConsola() {
-        StringStack myMethod = new StringStack(10);
-        Scanner s = new Scanner(System.in);
-        String myString = s.nextLine();
-        myMethod.method(myString);
-    }
-
-    private static void ex2Impossible() {
+    private static void ex2Impossible(){
         StringStack myMethod = new StringStack(10);
         myMethod.method("done. it's until impossible seems always It");
     }
 
-    private static void iCanDoIt() {
+    private static void iCanDoIt(){
         StringStack myStack = new StringStack(10);
         myStack.push("it!");
         myStack.push("do");
